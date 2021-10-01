366e0e75e283964b058e8b714cf48873ab74afbd
Maria Mireles <mireles335@gmail.com>
2021-10-01 17:42:27 -0500

    apollo server running/created get all users

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,12 @@
+const { User } = require('../models');
+
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find().sort({ createdAt: -1 });
+    },
+  },
+};
+
+module.exports = resolvers;
