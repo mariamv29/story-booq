2d2154538d961dd3d42a9a646c7ebe1b9ceecb70
Maria Mireles <mireles335@gmail.com>
2021-10-01 20:33:55 -0500

    typeDefs/ me[user]/books

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,16 +1,30 @@
 const {gql} = require("apollo-server-express");
 
 const typeDefs = gql`
+
+type Book {
+    _id: ID
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+}
+
 type User {
     _id: ID
     username: String
     email: String
     password: String
     bookCount: Int
-
+    savedBooks: [Book]
 }
 type Query {
+    me: User
     users: [User]
+    user(username: String!):User
   }`;
 
 module.exports = typeDefs;
+
