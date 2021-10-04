ac855f73424e34435dc5acb6bb1c17fdc3008bba
Maria Mireles <mireles335@gmail.com>
2021-10-04 00:00:39 -0500

    handled saved/remove books front end/ getting error

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,51 +1,49 @@
 import React from 'react';
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation} from "@apollo/client";
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 import { QUERY_ME } from '../utils/queries';
-// import { REMOVE_BOOK}  from "../utils/mutations"
+import { REMOVE_BOOK}  from "../utils/mutations"
 // import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-  // const [userData, setUserData] = useState({});
+  const [removeBook] = useMutation(REMOVE_BOOK);
 
-  // const removeBook = useMutation(REMOVE_BOOK);
-  const { loading, data } = useQuery(QUERY_ME);
+  const {  data } = useQuery(QUERY_ME);
   const userData = data?.me || [];
   console.log(userData);
 
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  // const handleDeleteBook = async (bookId) => {
-  //   const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-  //   if (!token) {
-  //     return false;
-  //   }
+  const userDataLength = Object.keys(userData).length
+  //create function that accepts the book's mongo _id value as param and deletes the book from the database
+  const handleDeleteBook = async (bookId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-  //   try {
-  //     const response = await removeBook({
-  //     variables: {id: bookId, token}
-  //     });
-  //   } 
+    if (!token) {
+      return false;
+    }
 
-  //     if (!response.ok) {
-  //       throw new Error('something went wrong!');
-  //     }
+    try {
+      const response = await removeBook({
+      variables: {id: bookId, token}
+      });
 
-  //     const updatedUser = await response.json();
-  //     setUserData(updatedUser);
-  //     // upon success, remove book's id from localStorage
-  //     removeBookId(bookId);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
 
-  // //if data isn't here yet, say so
-  // if (!userDataLength) {
-  //   return <h2>LOADING...</h2>;
-  // }
+
+      removeBookId(bookId);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  //if data isn't here yet, say so
+  if (!userDataLength) {
+    return <h2>LOADING...</h2>;
+  }
 
   return (
     <>
@@ -70,8 +68,8 @@ const SavedBooks = () => {
                   <p className='small'>Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
                   <Button className='btn-block btn-danger' 
-                  // onClick={() => 
-                  //   handleDeleteBook(book.bookId)}
+                  onClick={() => 
+                    handleDeleteBook(book.bookId)}
                     >
                     Delete this Book!
                   </Button>
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import { useMutation } from "@apollo/client"
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import {  searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { SAVE_BOOK } from '../utils/mutations'
+
 
 const SearchBooks = () => {
+const [saveBook ]= useMutation(SAVE_BOOK);
 
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
@@ -63,13 +67,11 @@ const SearchBooks = () => {
     if (!token) {
       return false;
     }
-
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      await saveBook({
+        variables: { input:bookToSave}
+      })
+ 
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
